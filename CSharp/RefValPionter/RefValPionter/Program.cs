b9df711b74a26bb2875802eea2504152a9f1b8bc
Anthony Sierra <aasierra@live.com>
2013-06-13 09:31:06 -0700

    First commit

---
+++ CSharp/RefValPionter/RefValPionter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefValPionter
+{
+    class Program
+    {
+        class Car
+        {
+            public int something;
+            public void ChangeCar(Car theCar)
+            {
+                theCar = new Car();
+                theCar.something = 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car temp = new Car();
+            temp.something = 1;
+            temp.ChangeCar(temp);
+            Console.WriteLine(temp.something);
+            Console.Read();
+        }
+
+    }
+}
